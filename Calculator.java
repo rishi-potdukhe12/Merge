7a6e49bf4ee00b93c4de8d2427d8ebdaee92a98d
rishi <rishipotdukhe84@gmail.com>
2022-07-12 12:09:32 +0530

    commit

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,15 @@ public class Calculator
     //from main branch
         return a+b;
     }
+
     public int sub(int a, int b){
     //from feature-a branch
         return a-b;
+
+   public int multiply(int a, int b){
+    //from feature-b branch
+        return a*b;
+
     }
 
 }
